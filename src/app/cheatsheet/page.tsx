2a5fe9d947a7d0b84a435527505343a0482e05e7
tonyghouse <tonyghouseofficial@gmail.com>
2024-05-30 00:50:32 +0530

    cheetsheet and guide added

--- src/app/cheatsheet/page.tsx
+++ src/app/cheatsheet/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function CheatSheetPage() {
   return (
-    <div>CheatSheetPage</div>
-  )
+    <>
+      <div className="my-3 w-full h-[100vh] border-[0.07rem] md:border-[0.09rem] border-[black] ">
+        <div className="grid grid-flow-row-dense grid-cols-3 grid-rows-3  gap-1">
+          <div className="h-[10rem] w-[15rem]  m-2 border">01</div>
+          <div className="h-[10rem] w-[15rem]  m-2 border">02</div>
+          <div className="h-[10rem] w-[15rem]  m-2 border">03</div>
+          <div className="h-[10rem] w-[15rem]    m-2 border">04</div>
+          <div className="h-[10rem] w-[15rem]   m-2 border">05</div>
+         
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default CheatSheetPage
+export default CheatSheetPage;
