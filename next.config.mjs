b2f186d04dac46495b59c20b36bb1d1a0bb346fc
tonyghouse <tonyghouseofficial@gmail.com>
2024-05-30 10:46:32 +0530

    disDir changed to out

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
-    distDir : "dist",
+    // distDir : "dist",
     images:{
         unoptimized: true
     }
